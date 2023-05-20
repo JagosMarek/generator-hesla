b283fade835c062d91b72e1f997705b26ec6d13f
Marek Jagoš <m.jagos@email.cz>
2023-05-20 07:37:16 +0200

    Just backup

--- src/main/java/RPG/Main.java
+++ src/main/java/RPG/Main.java
@@ -1,27 +1,52 @@
 package RPG;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        UI ui = new UI();
-        ui.addPassword();
-        ui.addPassword();
-        ui.addPassword();
-        ui.getAllPasswords();
-        ui.findPasswords();
-        ui.addPassword();
-        ui.deletePassword();
-        ui.getAllPasswords();
-        ui.deleteAllPasswords();
-        ui.getAllPasswords();
+        Scanner sc = new Scanner(System.in);
+        UI ui = new UI(sc);
+        String choice = "";
+        ui.welcomeScreen();
+        while (!choice.equals("6")){
+            System.out.println();
+            System.out.println("==============================");
+            System.out.println("Select action: ");
+            System.out.println("1 - addPassword");
+            System.out.println("2 - printAllPasswords");
+            System.out.println("3 - findPassword");
+            System.out.println("4 - deletePassword");
+            System.out.println("5 - deleteAllPasswords");
+            System.out.println("6 - end");
+            System.out.println("==============================");
+            choice = sc.nextLine().trim();
+            System.out.println();
 
-
-
-        //oštřit aby nebyla nula a max 50 ------ DONE
-        //nabídnout jaká znaky chce použít ------ DONE
-        //vrátit mu i sílu hesla ------ DONE
-        //nabídnout jestli chce do hesla zahrnout i svoje vlastní slovo
-        //schovat pasword pomocí ### ------ NOPE
-        //dodělat ui
+            switch (choice){
+                case "1":
+                    ui.addPassword();
+                    break;
+                case "2":
+                    ui.getAllPasswords();
+                    break;
+                case "3":
+                    ui.findPasswords();
+                    break;
+                case "4":
+                    ui.deletePassword();
+                    break;
+                case "5":
+                    ui.deleteAllPasswords();
+                    break;
+                case "6":
+                    //end
+                    break;
+                default:
+                    System.out.println("Invalid selection, please re-enter.");
+                    break;
+            }
+        }
+        System.out.println("Thank you for using random password generator :)");
     }
 }
--- src/main/java/RPG/UI.java
+++ src/main/java/RPG/UI.java
@@ -1,21 +1,24 @@
 package RPG;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class UI {
 
     private Generator generator;
-    private Scanner sc = new Scanner(System.in);
     private int passwordLength;
     private String charactersUsed;
+    private Scanner sc;
 
-    public UI() {
+    public UI(Scanner sc) {
         generator = new Generator();
+        this.sc = sc;
     }
 
     public void welcomeScreen() {
-        System.out.println("====================");
+        System.out.println("==============================");
         System.out.println("Welcome to random password generator!");
         System.out.println("You can create a new random password");
     }
@@ -62,19 +65,19 @@ public class UI {
                 userWord = sc.nextLine().trim();
                 generator.setCustomWord(userWord);
                 add++;
-                while (addd == 0){
-                    if(choice.equals("yes") || choice.equals("y")) {
+                while (addd == 0) {
+                    if (choice.equals("yes") || choice.equals("y")) {
                         System.out.println("Do you want to put your word at the beginning or at the end" +
                                 " [beginning = b | end = e]");
                         String beginnigOrEndChoice = sc.nextLine().toLowerCase().trim();
                         if (beginnigOrEndChoice.equals("b") || beginnigOrEndChoice.equals("beginning")) {
                             generator.setWordEndOrFront(beginnigOrEndChoice);
                             addd++;
-                        } else if(beginnigOrEndChoice.equals("e") || beginnigOrEndChoice.equals("end")) {
+                        } else if (beginnigOrEndChoice.equals("e") || beginnigOrEndChoice.equals("end")) {
                             generator.setWordEndOrFront(beginnigOrEndChoice);
                             addd++;
                         } else {
-                            System.out.println("Please try it again.");
+                            System.out.println("Incorrect entry, please re-enter.");
                         }
                     }
                 }
@@ -84,104 +87,104 @@ public class UI {
                 generator.setWordEndOrFront("");
                 add++;
             } else {
+                System.out.println("Incorrect entry, please re-enter.");
+            }
+        }
+    }
+
+    public void addPassword() {
+        System.out.println("Create a new random password.");
+        System.out.println("Choose a password length: ");
+        setPasswordLength();
+        System.out.println("Choose a characters you want to use: ");
+        setCharactersUsed();
+        setUserWord();
+        System.out.println("A new password was created. Your new password is below: ");
+        generator.addPassword(passwordLength, charactersUsed);
+        getPasswordStrength();
+    }
+
+    private void getPasswordStrength() {
+        int bothWords = generator.getNewPassword().length();
+        if (bothWords >= 1 && bothWords <= 4) {
+            System.out.println("Password strength: Very Weak");
+        } else if (bothWords >= 5 && bothWords <= 7) {
+            System.out.println("Password strength: Weak");
+        } else if (bothWords >= 8 && bothWords <= 9) {
+            System.out.println("Password strength: Good");
+        } else if (bothWords >= 10 && bothWords <= 11) {
+            System.out.println("Password strength: Strong");
+        } else {
+            System.out.println("Password strength: Very Strong Nice...");
+        }
+    }
+
+    public void getAllPasswords() {
+        ArrayList<Password> passwords = generator.findAllPasswords();
+        passwords.sort((s1, s2) -> s1.getPasswordLength() - s2.getPasswordLength());
+        if (passwords.size() < 1) {
+            System.out.println("No passwords found");
+        } else {
+            System.out.println("All passwords sorted by length are: ");
+            for (Password password : passwords) {
+                System.out.println(password + "   |with length: " + password.getPasswordLength());
+            }
+        }
+    }
+
+    public void findPasswords() {
+        System.out.println("I can find all passwords of the same length");
+        setPasswordLength();
+        ArrayList<Password> passwords = generator.findPassword(passwordLength);
+        if (passwords.size() < 1) {
+            System.out.println("No passwords found with length: " + passwordLength);
+        } else {
+            System.out.println("Found these passwords with length: " + passwordLength);
+            for (Password password : passwords) {
+                System.out.println(password);
+            }
+        }
+    }
+
+    public void deleteAllPasswords() {
+        System.out.println("Do you want to delete all passwords? [Yes/No]");
+        String choice = "";
+        int delete = 0;
+        while (delete == 0) {
+            choice = sc.nextLine().toLowerCase().trim();
+            if (choice.equals("yes") || choice.equals("y")) {
+                generator.deleteALLPasswords(true);
+                System.out.println("All passwords were deleted!");
+                delete++;
+            } else if (choice.equals("no") || choice.equals("n")) {
+                System.out.println("Okey passwords won't be deleted ");
+                delete++;
+            } else {
                 System.out.println("Please try it again.");
             }
         }
     }
 
-        public void addPassword () {
-            System.out.println();
-            System.out.println("==============================");
-            System.out.println("Create a new random password.");
-            System.out.println("Choose a password length: ");
-            setPasswordLength();
-            System.out.println("Choose a characters you want to use: ");
-            setCharactersUsed();
-            System.out.println();
-            System.out.println("A new password was created.");
-            setUserWord();
-            generator.addPassword(passwordLength, charactersUsed);
-            getPasswordStrength();
-            System.out.println("==============================");
-            System.out.println();
-        }
-
-        private void getPasswordStrength (){
-        int bothWords = generator.getNewPassword().length();
-            if (bothWords >= 1 && bothWords <= 4) {
-                System.out.println("Password strength: Very Weak");
-            } else if (bothWords >= 5 && bothWords <= 7) {
-                System.out.println("Password strength: Weak");
-            } else if (bothWords >= 8 && bothWords <= 9) {
-                System.out.println("Password strength: Good");
-            } else if (bothWords >= 10 && bothWords <= 11) {
-                System.out.println("Password strength: Strong");
+    public void deletePassword() {
+        System.out.println("Enter the length of the password. Passwords with this length will be deleted");
+        setPasswordLength();
+        System.out.println("Do you want to delete password with this length? " + passwordLength + " [Yes/No]");
+        String choice = "";
+        int delete = 0;
+        while (delete == 0) {
+            choice = sc.nextLine().toLowerCase().trim();
+            if (choice.equals("yes") || choice.equals("y")) {
+                generator.deletePassword(passwordLength);
+                System.out.println("All passwords with length: " + passwordLength + " were deleted!");
+                delete++;
+            } else if (choice.equals("no") || choice.equals("n")) {
+                System.out.println("Okey passwords won't be deleted ");
+                delete++;
             } else {
-                System.out.println("Password strength: Very Strong Nice...");
-            }
-        }
-
-        public void getAllPasswords () {
-            ArrayList<Password> passwords = generator.findAllPasswords();
-            System.out.println("All passwords are: \n");
-            for (Password password : passwords) {
-                System.out.println(password + " length: " + password.getPasswordLength());
-            }
-        }
-
-        public void findPasswords () {
-            System.out.println("I can find all passwords of the same length");
-            setPasswordLength();
-            ArrayList<Password> passwords = generator.findPassword(passwordLength);
-            if (passwords.size() < 1) {
-                System.out.println("No passwords found");
-            } else {
-                System.out.println("Found these passwords with length: " + passwordLength);
-                for (Password password : passwords) {
-                    System.out.println(password);
-                }
-            }
-        }
-
-        public void deleteAllPasswords () {
-            System.out.println("Do you want to delete all passwords? [Yes/No]");
-            String choice = "";
-            int delete = 0;
-            while (delete == 0) {
-                choice = sc.nextLine().toLowerCase().trim();
-                if (choice.equals("yes") || choice.equals("y")) {
-                    generator.deleteALLPasswords(true);
-                    System.out.println("All passwords were deleted!");
-                    delete++;
-                } else if (choice.equals("no") || choice.equals("n")) {
-                    System.out.println("Okey passwords won't be deleted ");
-                    delete++;
-                } else {
-                    System.out.println("Please try it again.");
-                }
-            }
-        }
-
-        public void deletePassword () {
-            System.out.println("Enter the length of the password. Passwords with this length will be deleted");
-            setPasswordLength();
-            System.out.println("Do you want to delete password with this length? " + passwordLength + " [Yes/No]");
-            String choice = "";
-            int delete = 0;
-            while (delete == 0) {
-                choice = sc.nextLine().toLowerCase().trim();
-                if (choice.equals("yes") || choice.equals("y")) {
-                    generator.deletePassword(passwordLength);
-                    System.out.println("All passwords with length: " + passwordLength + " will be deleted!");
-                    delete++;
-                } else if (choice.equals("no") || choice.equals("n")) {
-                    System.out.println("Okey passwords won't be deleted ");
-                    delete++;
-                } else {
-                    System.out.println("Please try it again.");
-                }
+                System.out.println("Please try it again.");
             }
         }
     }
+}
 
 
